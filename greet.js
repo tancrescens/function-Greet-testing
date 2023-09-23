e1bfe643cd3d779c22d8f36883ab6b8dbd4aceaf
Crescens <2e3crescens@gmail.com>
2023-09-24 02:34:06 +0800

    finished creating and testing greet()

--- greet.js
+++ greet.js
@@ -3,10 +3,29 @@ let name = 'Elizabeth';
 function greet(name) {
     let result = '';
 
-    if(typeof name !== "string") {
+    // when input is (2)null
+    if(name === undefined || name === null || (typeof name !== "string" && !(Array.isArray(name)))) {
         result = 'Hello there!';
         return result;
-    }else {
+    }
+    // when input is in (3)ALL CAPS
+    else if(/^[A-Z]+$/.test(name)) {
+        result = 'HELLO ' + name;
+        return result;
+    }
+    // when input (4) has names in array form
+    else if (typeof name == "array") {
+        result = 'Hello, ';
+        name.forEach((item) => {
+            result += item + ',';
+        })
+        result.trim();
+        result.slice(0, -1);
+        console.log(result);
+        return result;
+    }
+    //when input is (1) valid string
+    else {
         result = 'Hello, ' + name;
         return result;
     }
--- greet.test.js
+++ greet.test.js
@@ -1,13 +1,25 @@
-const greet = require('./greet.js');
+const greet = require('./greet.js')
 
-// test 1: testing output is correct
+// test 1: testing output is correct if valid string input
 test('test greet() functioning if a valid string is input', () => {
     expect(greet('Elizabeth')
-    ).toBe('Hello, Elizabeth');
+    ).toBe('Hello, Elizabeth')
 })
 
 // test 2: testing input for names that are not string
 test('if name in greet(name) is not string, print "Hello there!"', () => {
     expect(greet(123)
     ).toBe('Hello there!')
+})
+
+// test 3: SHOUT BACK if input is all caps
+test('if name in greet(name) is ALL CAPS, print "HELLO NAME!"', () => {
+    expect(greet('JOSE')
+    ).toBe('HELLO JOSE')
+})
+
+// test 4: testing to output appropriate number of names when names >=2
+test('if name in greet(name) is in an array, print "Hello, name1, name2 ..."', () => {
+    expect(greet(["Joash", "Job"])
+    ).toBe('Hello, Joash,Job')
 })
